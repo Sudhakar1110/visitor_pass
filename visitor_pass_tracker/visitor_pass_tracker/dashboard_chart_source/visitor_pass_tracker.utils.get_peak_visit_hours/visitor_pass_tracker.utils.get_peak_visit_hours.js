15d8e04c783e153a195feb5f6a0903a59ec7a1ba
Sujai <sujaiit0696@gmail.com>
2026-08-05 15:49:16 +0530

    fix: ship dashboard/chart/notification fixtures as non-standard so install works without developer_mode; add Peak Visit Hours chart source JS

---
+++ visitor_pass_tracker/visitor_pass_tracker/dashboard_chart_source/visitor_pass_tracker.utils.get_peak_visit_hours/visitor_pass_tracker.utils.get_peak_visit_hours.js
@@ -0,0 +1,5 @@
+frappe.provide("frappe.dashboards.chart_sources");
+frappe.dashboards.chart_sources["visitor_pass_tracker.utils.get_peak_visit_hours"] = {
+	method: "visitor_pass_tracker.utils.get_peak_visit_hours",
+	filters: [],
+};
